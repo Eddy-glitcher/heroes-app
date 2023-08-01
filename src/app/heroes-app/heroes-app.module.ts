5133816398299d6161e9e6c5002f69fb0918b6ad
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-07-31 20:07:23 -0500

    Feat: Added the carousel module and slider component

--- src/app/heroes-app/heroes-app.module.ts
+++ src/app/heroes-app/heroes-app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { HeroesAppRoutingModule } from './heroes-app-routing.module';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { HeroesAppRoutingModule } from './heroes-app-routing.module';
+import { HeroSliderComponent } from './components/hero-slider/hero-slider.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     CharactersPageComponent,
+    HeroSliderComponent,
   ],
   imports: [
     CommonModule,
-    HeroesAppRoutingModule
+    HeroesAppRoutingModule,
+    CarouselModule
   ]
 })
 export class HeroesAppModule { }
