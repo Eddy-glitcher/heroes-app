f0aaf87d8f8b363f877c5b56b9073c7ace89ac9b
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-08-15 21:25:17 -0500

    Feat: Added the method to get marvel heroe list

--- src/app/heroes-app/services/marvel-heroes.service.ts
+++ src/app/heroes-app/services/marvel-heroes.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { MarvelHeroes } from '../interfaces/marvel-heroes';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, retry, throwError } from 'rxjs';
+import { MarvelHeroe, MarvelHeroes } from '../interfaces/marvel-heroes';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +13,15 @@ export class MarvelHeroesService {
   apiKey     : string = environment.apiKey;
   apiHash    : string = environment.apiHash;
 
-  getMarvelHeroes(): Observable<MarvelHeroes>{
-    const apiUrl = `${this.apiBaseUrl}/characters?ts=1&apikey=${this.apiKey}&hash=${this.apiHash}&limit=100`;
-    return this.httpRequest.get<MarvelHeroes>(apiUrl);
+  // To get the heroe list of marvel api
+  getMarvelHeroes(): Observable<MarvelHeroe[]>{
+    const apiUrl = `${this.apiBaseUrl}/characters?ts=1&apikey=${this.apiKey}&hash=${this.apiHash}&limit=100`; // To build the end point api request
+    return this.httpRequest.get<MarvelHeroes>(apiUrl).pipe(
+      retry(1), // If the http request fails is retried once
+      map( (resp: MarvelHeroes) => resp.data.results), // Maps the results items to get the heroes
+      catchError( (error: HttpErrorResponse) => throwError(()=>error)) // Control the Http errors
+    );
   }
-
+  
   constructor(private httpRequest : HttpClient) { }
 }
