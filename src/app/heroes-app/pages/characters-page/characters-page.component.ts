e5a1b9d8c353b504ee219eaead36e85e6b05a591
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-08-16 19:20:39 -0500

    Feat: Added marvel heroes method to get them

--- src/app/heroes-app/pages/characters-page/characters-page.component.ts
+++ src/app/heroes-app/pages/characters-page/characters-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MarvelHeroesService } from '../../services/marvel-heroes.service';
+import { MarvelHeroe } from '../../interfaces/marvel-heroes';
 
 @Component({
   selector: 'app-characters-page',
   templateUrl: './characters-page.component.html',
   styleUrls: ['./characters-page.component.scss']
 })
-export class CharactersPageComponent {
+export class CharactersPageComponent implements OnInit{
 
+  heroeList : MarvelHeroe[] = [];
+  isHttpRequestFails : boolean = false;
+
+  constructor(private heroService : MarvelHeroesService){}
+
+  ngOnInit(): void {
+    this.heroService.getMarvelHeroes().subscribe({
+      next: (heroeArrList) => {this.heroeList = heroeArrList}, // Get the herolist if there are no errors.
+      error: (error : HttpErrorResponse) => { this.isHttpRequestFails = true;} // To show the error message.
+    });
+  }
 }
