86c036814fdf35b58468feb2baec2c34f02ea074
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-08-20 16:02:49 -0500

    Feat: Updated the image source for hero-image pipe

--- src/app/heroes-app/pipes/hero-image-not-found.pipe.ts
+++ src/app/heroes-app/pipes/hero-image-not-found.pipe.ts
@@ -9,7 +9,7 @@ export class HeroImageNotFoundPipe implements PipeTransform {
 
     if (image == 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
       || image == 'http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708.gif') {
-      return 'https://i.postimg.cc/R0WYKH6t/Group-1171275177-1.png';
+      return 'https://i.postimg.cc/R0WYKH6t/marvel-image-not-found.png';
     }
     return image;
   }
