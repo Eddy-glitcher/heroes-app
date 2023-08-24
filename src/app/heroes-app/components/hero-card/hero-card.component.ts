ce2076b282c586750392f79c82dd93daf6ed09f2
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-08-23 19:00:55 -0500

    Feat: Added card skeleton loader

--- src/app/heroes-app/components/hero-card/hero-card.component.ts
+++ src/app/heroes-app/components/hero-card/hero-card.component.ts
@@ -1,10 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MarvelHeroe } from '../../interfaces/marvel-heroes';
 @Component({
   selector: 'app-hero-card',
   templateUrl: './hero-card.component.html',
   styleUrls: ['./hero-card.component.scss']
 })
-export class HeroCardComponent{
+export class HeroCardComponent implements OnInit, OnDestroy{
   @Input() heroe!: MarvelHeroe;
+  @Input() isHeroListLoading?: boolean;
+
+  animation : string = 'pulse';
+  count: number = 1;
+  widthHeightSizeInPixels: number = 50;
+  intervalId: number | null = null;
+
+  skeletonItemsLoadingStyles = {
+    width: '100%',
+    height: '100%',
+    margin: '0px auto',
+    'background-color': '#232A30',
+    'animation-duration': '2s'
+  }
+
+  ngOnInit(): void {
+    this.intervalId = window.setInterval(() => {
+      this.animation = this.animation === 'pulse' ? 'progress-dark' : 'pulse';
+      this.count = this.count === 1 ? 5 : 1;
+      this.widthHeightSizeInPixels =
+        this.widthHeightSizeInPixels === 50 ? 100 : 50;
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
