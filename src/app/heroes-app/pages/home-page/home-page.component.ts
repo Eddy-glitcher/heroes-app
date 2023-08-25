4ac571079962c93124250b534c17943763d50b5c
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-08-24 19:21:59 -0500

    Docs: Added the structural Documentation

--- src/app/heroes-app/pages/home-page/home-page.component.ts
+++ src/app/heroes-app/pages/home-page/home-page.component.ts
@@ -8,6 +8,7 @@ import { HeroeImages } from '../../interfaces/heroe-images';
 })
 export class HomePageComponent {
 
+  // This is the list of hero images that will be shown in the slider
   heroImageList: HeroeImages[] = [
     { id: 'first-hero-image' , name: 'Spiderman', alt: 'Spiderman Image', imageUrl: 'https://i.postimg.cc/QdB5wbCH/Spiderman.jpg' },
     { id: 'second-hero-image', name: 'Iron Man' , alt: 'Iron Man Image' , imageUrl: 'https://i.postimg.cc/XJ9bXTvt/Iron-Man.jpg'  },
