2c7ff5cfd88d463cf5c10c5e9b8ff55ba48812de
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-08-27 16:20:00 -0500

    Feat: Added the debounceTime functionality

--- src/app/heroes-app/components/debounce-hero-list/debounce-hero-list.component.ts
+++ src/app/heroes-app/components/debounce-hero-list/debounce-hero-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MarvelHero } from '../../interfaces/marvel-heroes';
+import { debounceTime, tap } from 'rxjs';
 
 @Component({
   selector: 'app-debounce-hero-list',
@@ -8,33 +10,76 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class DebounceHeroListComponent {
 
-  isFocused: boolean = false;
-  isFormFieldEmpty : boolean = false;
+  // Receives the hero List from the parent
+  @Input() heroList : MarvelHero[] = [];
+  debounceHeroList  : MarvelHero[] = [];
 
-  onFocus(): void {
-    this.isFocused = true;
-  }
-
-  onBlur(): void {
-    this.isFocused = false;
-  }
+  isTextBoxEmpty       : boolean = false;
+  isTextBoxFocused     : boolean = false;
+  isHeroSearchActive   : boolean = false;
+  isHeroSearchedExist  : boolean = false;
+  showHeroListOptions  : boolean = false;
 
   heroForm: FormGroup = this.fb.group({
     name : ['']
-  })
+  });
 
   validateEmptyFormField(campo: string): void{
-    if(this.heroForm.controls[campo].value.length >0){
-      this.isFormFieldEmpty = true;
-    }else{
-      this.isFormFieldEmpty = false;
-    }
+    this.heroForm.controls[campo].valueChanges.pipe(
+      tap(()=>{
+
+      (this.heroForm.controls[campo].value.length !== 0) ?
+                            this.isTextBoxEmpty = true :
+                            this.isTextBoxEmpty = false;
+
+      (this.heroForm.controls[campo].value.length >= 0 && this.debounceHeroList.length == 0 &&
+      !this.isHeroSearchedExist)     ?
+      this.isHeroSearchActive = true :
+      this.isHeroSearchActive = false;
+
+      }),
+      debounceTime(300),
+      ).subscribe( (value: string) =>{
+        this.debounceHeroList =  this.heroList.filter((hero): any =>{
+          if(value){
+            return hero.name.toLowerCase().includes(value.toLowerCase());
+          }
+        });
+
+        if(this.heroForm.controls[campo].value.length !== 0 && this.debounceHeroList.length == 0 && this.isHeroSearchActive == false){
+          this.isHeroSearchedExist = true;
+          this.isHeroSearchActive  = false;
+        }else{
+          this.isHeroSearchedExist = false;
+          this.isHeroSearchActive  = false;
+        }
+      });
   }
 
-  cleanFormField(campo: string): void{
-    this.heroForm.controls[campo].reset();
-    this.isFormFieldEmpty = false;
+  selectHero(hero : string){
+    this.heroForm.controls['name'].setValue(hero);
+    this.showHeroListOptions = false;
   }
+
+
+  cleanFormField(campo: string): void{
+    this.isTextBoxEmpty      = false;
+    this.isHeroSearchActive  = false;
+    this.isHeroSearchedExist = false;
+    this.debounceHeroList    = [];
+    this.heroForm.controls[campo].reset();
+  }
+
+    // To manipulate de html click event
+    @ViewChild('divElement') divElement!: ElementRef<HTMLElement>;
+    @HostListener('document:click', ['$event.target'])
+    // To hide the suggested Heroes list when the user cliks outside the text box
+    onClick(targetElement : Node): void {
+      const clickedInside = this.divElement.nativeElement.contains(targetElement);
+      if (!clickedInside) {
+        this.showHeroListOptions = false;
+      }
+    }
 
   constructor(private fb: FormBuilder) { }
 }
