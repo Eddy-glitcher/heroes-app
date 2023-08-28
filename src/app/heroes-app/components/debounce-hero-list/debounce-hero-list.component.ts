6fd0fa82de5a3dc555f23f678fc1324fc93b6aaa
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-08-27 20:16:05 -0500

    Docs: Added the functionality and structural documentation

--- src/app/heroes-app/components/debounce-hero-list/debounce-hero-list.component.ts
+++ src/app/heroes-app/components/debounce-hero-list/debounce-hero-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { MarvelHero } from '../../interfaces/marvel-heroes';
 import { debounceTime, tap } from 'rxjs';
+import { MarvelHero } from '../../interfaces/marvel-heroes';
 
 @Component({
   selector: 'app-debounce-hero-list',
@@ -14,38 +14,45 @@ export class DebounceHeroListComponent {
   @Input() heroList : MarvelHero[] = [];
   debounceHeroList  : MarvelHero[] = [];
 
-  isTextBoxEmpty       : boolean = false;
-  isTextBoxFocused     : boolean = false;
-  isHeroSearchActive   : boolean = false;
-  isHeroSearchedExist  : boolean = false;
-  showHeroListOptions  : boolean = false;
+  isTextBoxEmpty       : boolean = false; // To displays the searchbar cleaner icon
+  isTextBoxFocused     : boolean = false; // To displays styles for searchbar input, button and icons
+  isHeroSearchActive   : boolean = false; // To displays the search message
+  isHeroSearchedExist  : boolean = false; // To displays the not found message
+  showHeroListOptions  : boolean = false; // To displays the heroes List
 
+  // To save the heroes name when the user types it in search box
   heroForm: FormGroup = this.fb.group({
     name : ['']
   });
 
+  // To search the hero in the list of heroes
   searchHeroByName(heroName: string): void{
     this.heroForm.controls[heroName].valueChanges.pipe(
       tap(()=>{
 
+      // To show o hide the searchbar cleaner icon
       (this.heroForm.controls[heroName].value.length !== 0) ?
                             this.isTextBoxEmpty = true :
                             this.isTextBoxEmpty = false;
 
+      // To show or hide the search message when the user types it in the search box
       (this.heroForm.controls[heroName].value.length >= 0 && this.debounceHeroList.length == 0 &&
       !this.isHeroSearchedExist)     ?
       this.isHeroSearchActive = true :
       this.isHeroSearchActive = false;
 
       }),
+      // This debounceTime function delays the function execution
       debounceTime(300),
       ).subscribe( (value: string) =>{
+        // If the entrie it´s included in the heroList, the hero name will be saved in the debounceList
         this.debounceHeroList =  this.heroList.filter((hero): boolean | void =>{
           if(value){
             return hero.name.toLowerCase().includes(value.toLowerCase());
           }
         });
 
+        // To show or hide the search message and not found error message
         if(this.heroForm.controls[heroName].value.length !== 0 && this.debounceHeroList.length == 0 && this.isHeroSearchActive == false){
           this.isHeroSearchedExist = true;
           this.isHeroSearchActive  = false;
@@ -56,12 +63,13 @@ export class DebounceHeroListComponent {
       });
   }
 
+  // To select the hero and execute the search event emmiter to find the hero
   selectHero(hero : string, heroFormEntrie : string): void{
     this.heroForm.controls[heroFormEntrie].setValue(hero);
     this.showHeroListOptions = false;
   }
 
-
+  // To clean the search box and restart the heroes List
   cleanHeroFormEntrie(heroFormEntrie: string): void{
     this.isTextBoxEmpty      = false;
     this.isHeroSearchActive  = false;
